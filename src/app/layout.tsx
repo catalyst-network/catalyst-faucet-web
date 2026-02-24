4d97b306a20cbd316e1b7547a568b857e1937bdf
TheNewAutonomy <darren@ourfamily.io>
2026-02-24 20:04:42 +0000

    Update branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="app-backdrop" aria-hidden="true" />
         {children}
       </body>
     </html>
